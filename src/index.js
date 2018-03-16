83e889a382ccaae4ec4339c86223aa16dae5c004
Matthew <matthewjkane@live.com>
2018-03-16 16:34:05 -0700

    Scott told me to

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+const user = {
+    firstName: "Billy",
+    lastName: "Bob"
+}
+
+const greeting = <h1>This is an ok greeting to {takeMeNow(user)}</h1>
+
+function takeMeNow(user){
+    return `${user.firstName} ${user.lastName}`
+}
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
+
+
+
